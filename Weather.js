114d068b8d3f89261c0adc2acd57507360ca2f30
olyesung <olyesung@gmail.com>
2021-09-13 19:41:45 +0900

    1.4 Titles and Subtitles

--- Weather.js
+++ Weather.js
@@ -5,49 +5,45 @@ import { LinearGradient } from "expo-linear-gradient";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const weatherOptions = {
+  Thunderstorm: {
+    iconName: "weather-lightning",
+    gradient: ["#373B44", "#4286f4"],
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["#FF7300", "#FEF253"],
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+  },
   Haze: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+  },
+  Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
   },
-  Thunderstorm: {
-    iconName: "",
-    gradient: [],
-  },
-  Drizzle: {
-    iconName: "",
-    gradient: [],
-  },
-  Rain: {
-    iconName: "",
-    gradient: [],
-  },
-  Snow: {
-    iconName: "",
-    gradient: [],
-  },
-  Atmosphere: {
-    iconName: "",
-    gradient: [],
-  },
-  Clear: {
-    iconName: "",
-    gradient: [],
-  },
-  Clouds: {
-    iconName: "",
-    gradient: [],
-  },
-  Haze: {
-    iconName: "",
-    gradient: [],
-  },
-  Mist: {
-    iconName: "",
-    gradient: [],
-  },
   Dust: {
-    iconName: "",
-    gradient: [],
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
   },
 };
 
